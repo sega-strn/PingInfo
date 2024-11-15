6fea98a7eb9bf11fe5ccd93d18bb734ce057656e
Sergey <segadev85@gmail.com>
2024-11-15 13:10:18 +0300

    start ping

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func ping(url string, respCh chan int, errCh chan error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		errCh <- err
+		return
+	}
+	respCh <- resp.StatusCode
+
+}
+
+func main() {
+	path := flag.String("file", "url.txt", "path to URL file")
+	flag.Parse()
+	file, err := os.ReadFile(*path)
+	if err != nil {
+		panic(err.Error())
+	}
+	urlSlice := strings.Split(string(file), "\n")
+
+	respCh := make(chan int)
+	errCh := make(chan error)
+
+	for _, url := range urlSlice {
+		go ping(url, respCh, errCh)
+	}
+	for range urlSlice {
+		select {
+		case res := <-respCh:
+			fmt.Println(res)
+		case errRes := <-errCh:
+			fmt.Println(errRes)
+
+		}
+
+	}
+}
